1980fd909db4dc1f6cc99da263af3b56155a039f
Emilia Paredes <eparedes@somoswoxi.com>
2024-06-16 20:20:22 -0300

    instalacion de vue

---
+++ src/app/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello World con VueJS!</h1>
+</template>
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,4 @@
-alert('Vuejs goes here')
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
